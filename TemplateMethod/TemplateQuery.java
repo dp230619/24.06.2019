d3209db5738bc8e7aa25ccf604b62ccca389e066
dp230619 <52121481+dp230619@users.noreply.github.com>
2019-06-24 16:15:49 +0300

    Add files via upload

---
+++ TemplateMethod/TemplateQuery.java
@@ -0,0 +1,17 @@
+
+public abstract class TemplateQuery {
+
+	protected abstract void createDbConnection();
+	protected abstract void openConnection();
+	protected abstract void runSqlQuery();
+	protected abstract void closeConnection();
+	
+	public void run()
+	{
+		createDbConnection();
+		openConnection();
+		runSqlQuery();
+		closeConnection();
+	}
+	
+}
