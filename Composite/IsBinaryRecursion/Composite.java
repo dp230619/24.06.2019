9e60333e937632b1c7d099027277f2a95446460f
dp230619 <52121481+dp230619@users.noreply.github.com>
2019-06-24 10:22:25 +0300

    Add files via upload

---
+++ Composite/IsBinaryRecursion/Composite.java
@@ -0,0 +1,42 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class Composite extends Component{
+
+	List<Component> children = new ArrayList<Component>(); 
+	
+	public Composite(String name) {
+		super(name);
+	}
+
+	@Override
+	public void addChild(Component c) {
+		children.add(c);
+	}
+
+	@Override
+	public void removeChild(Component c) {
+		children.remove(c);	
+	}
+
+	@Override
+	public List<Component> getChilds() {
+		return children;
+	}
+
+	@Override
+	public void draw(String space) {
+		System.out.println(space + this.name);
+		for (Component c : this.children) {
+			c.draw(space + "    ");
+		}
+	}
+
+	@Override
+	public boolean isBinary() {
+		return this.children.size() == 0 || this.children.size() == 2;
+	}
+	
+	
+
+}
---
+++ Composite/IsBinaryRecursion/Main.java
@@ -0,0 +1,52 @@
+import javax.naming.OperationNotSupportedException;
+
+public class Main {
+
+	public static void main(String[] args) throws OperationNotSupportedException {
+
+		Component root = new Composite("Canvas root");
+		Component circle1 = new Leaf("Circle1");
+		Component rectangle1 = new Leaf("Rectangle1");
+		root.addChild(circle1);
+		root.addChild(rectangle1);
+		
+		Component container1 = new Composite("container1");
+		Component circle2 = new Leaf("Circle2");
+		Component circle3 = new Leaf("Circle3");
+		
+		container1.addChild(circle2);
+		container1.addChild(circle3);
+		
+		root.addChild(container1);
+		
+		root.draw("");
+		
+		System.out.println(isBinary(root));
+		
+		
+		Component root2 = new Composite("Canvas root");
+		Component circle2_1 = new Leaf("circle2_1");
+		Component rectangle2_1 = new Leaf("rectangle2_1");
+		//Component rectangle2_1_1 = new Leaf("rectangle2_1");
+		root2.addChild(circle2_1);
+		root2.addChild(rectangle2_1);
+		//root2.addChild(rectangle2_1_1);
+		
+		System.out.println(isBinary(root2));
+		
+	}
+	
+	private static boolean isBinary(Component c)
+	{
+		if (!c.isBinary())
+			return false;
+		
+		if (c.getChilds() != null && c.getChilds().size() == 2) // can skip this check: c.getChilds().size() == 2
+		{
+			return isBinary(c.getChilds().get(0)) && isBinary(c.getChilds().get(0)); 
+		}
+		
+		return true;
+	}
+
+}
