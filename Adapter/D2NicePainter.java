81799d002f4d7590fbbb8e3b9f8e853d9e866389
dp230619 <52121481+dp230619@users.noreply.github.com>
2019-06-24 13:07:31 +0300

    Add files via upload

---
+++ Adapter/D2NicePainter.java
@@ -0,0 +1,11 @@
+
+public class D2NicePainter implements D2Painter{
+
+	@Override
+	public void paint2DShape(D2Shape s) {
+		
+		System.out.println("Printing nicely " + s);
+		
+	}
+
+}
---
+++ Adapter/D3NicePainter.java
@@ -0,0 +1,10 @@
+
+public class D3NicePainter implements D3Painter{
+
+	@Override
+	public void paint3DShape(D3Shape s) {
+		System.out.println("D3 is printing not so nice ... " + s);
+		
+	}
+
+}
